733fe544a013f63ce9f597f1aaae62770e9778ca
Majstor <76176291+wmdhosting@users.noreply.github.com>
2021-01-15 17:39:24 +0200

    Update SectionField.js

--- src/assetbundles/sectionfield/dist/js/SectionField.js
+++ src/assetbundles/sectionfield/dist/js/SectionField.js
@@ -1,105 +1,87 @@
-const idAllowed = '#types-wmd-sectionandproducttype-fields-SectionField-allowedSections-field';
-const idExclude = '#types-wmd-sectionandproducttype-fields-SectionField-excludedSections-field';
-const idSelectAllInput = '#types-wmd-sectionandproducttype-fields-SectionField-selectAll'
-let countCheckedAllow = $(`${idAllowed} input:checkbox:checked`).length;
-let countAllAllow = $(`${idAllowed} input:checkbox`).length;
+const idAllowed = $(document).find(`[data-ww="selectAll"]`).find("label");
+
+$(idAllowed).click(function () {
+    let checked = $(this).parent().find(".checkbox").is(":checked");
+    
+    let idSelectAllInput = $(this).parent().parent().parent().find(`[data-ww="allowedSections"]`);
+
+    let excludedSections = $(this).parent().parent().parent().find(`[data-ww="excludedSections"]`);
+
+    if (checked === true) {
+        $(idSelectAllInput).find(".checkbox").each(function (i, e) {
+            $(e).prop('checked', false);
+            $(e).css("pointer-events", "unset");
+        });
+
+        $(excludedSections).css("display", "none");
+
+        $(idSelectAllInput).find("label").each(function (i, e) {
+            $(e).css("opacity", "1");
+            $(e).css("pointer-events", "unset");
+        });
+
+    } else {
+        $(idSelectAllInput).find(".checkbox").each(function (i, e) {
+            $(e).prop('checked', true);
+            $(e).css("pointer-events", "none");
+        });
+
+        $(excludedSections).css("display", "block");
+
+        $(idSelectAllInput).find("label").each(function (i, e) {
+            $(e).css("opacity", "0.5");
+            $(e).css("pointer-events", "none");
+        });
+    }
+});
 
 
-//check initial status select all
-if (countCheckedAllow === countAllAllow) {
-    $(`${idSelectAllInput}`).prop('checked', true);
-} else {
-    $(`${idSelectAllInput}`).prop('checked', false);
+
+function checkSelectAll() {
+    let c = $(document).find(`[data-ww="selectAll"]`);
+
+    c.each(function () {
+        let checked;
+
+        let checkType = $(this).parent().parent().parent().parent().parent().parent().parent().parent().attr('class')
+
+        let  idSelectAllInput = $(this).parent().parent().parent().find(`[data-ww="allowedSections"]`);
+
+        if (checkType === "input ltr") {
+            checked = $(this).parent().find(".checkbox").is(":checked");
+        } else {
+            checked = $(this).find(".checkbox").is(":checked");
+        }
+
+        let excludedSections = $(this).parent().parent().parent().find(`[data-ww="excludedSections"]`);
+
+        if (checked === false) {
+            $(idSelectAllInput).find(".checkbox").each(function (i, e) {
+                $(e).prop('checked', false);
+                $(e).css("pointer-events", "unset");
+            });
+
+            $(excludedSections).css("display", "none");
+
+            $(idSelectAllInput).find("label").each(function (i, e) {
+                $(e).css("opacity", "1");
+                $(e).css("pointer-events", "unset");
+            });
+
+        } else {
+            $(idSelectAllInput).find(".checkbox").each(function (i, e) {
+                $(e).prop('checked', true);
+                $(e).css("pointer-events", "none");
+            });
+
+            $(excludedSections).css("display", "block");
+
+            $(idSelectAllInput).find("label").each(function (i, e) {
+                $(e).css("opacity", "0.5");
+                $(e).css("pointer-events", "none");
+            });
+        }
+    })
 }
 
-//change chackboxes allow and exclude after click on select all
-$(`${idSelectAllInput}`).click(function(){
-    if ($(this).is(':checked')){
-        $(`${idAllowed} input:checkbox`).prop('checked', true);
-        $(`${idExclude} input:checkbox`).prop('checked', false);
-    } else {
-        $(`${idAllowed} input:checkbox`).prop('checked', false);
-        $(`${idExclude} input:checkbox`).prop('checked', true);
-    }
-});
-
-//
-$(`${idAllowed} input:checkbox`).click(function(){
-
-    if ($(this).is(':checked')){
-        let countCheckedAllow = $(`${idAllowed} input:checkbox:checked`).length;
-        if (countCheckedAllow === countAllAllow) {
-            $(`${idSelectAllInput}`).prop('checked', true);
-        } else {
-            $(`${idSelectAllInput}`).prop('checked', false);
-        }
-    } else {
-        let countCheckedAllow = $(`${idAllowed} input:checkbox:checked`).length;
-        if (countCheckedAllow === countAllAllow) {
-            $(`${idSelectAllInput}`).prop('checked', true);
-        } else {
-            $(`${idSelectAllInput}`).prop('checked', false);
-        }
-    }
-});
-
-$(`${idExclude} input:checkbox`).click(function(){
-    let countCheckedAllow = $(`${idExclude} input:checkbox:checked`).length;
-    if (countCheckedAllow > 0) {
-        $(`${idSelectAllInput}`).prop('checked', false);
-    } else {
-        $(`${idSelectAllInput}`).prop('checked', true);
-    }
-});
-
-$('input:checkbox').click(function(){
-
-    let name = $(this).parent().find("label").text();
-
-    if ($(this).is(':checked')){
-        $('#settings').find("fieldset").each((i, e)=>{
-            $(e).find('div').each((i, e) => {
-                $(e).find('label').each((i,e) => {
-                    if ($(e).text() === name) {
-                        $(e).parent().find('input').prop('checked', false);
-                        $(this).parent().find('input').prop('checked', true);
-                    }
-                })
-            })
-        })
-    } else {
-        $('#settings').find("fieldset").each((i, e)=>{
-            $(e).find('div').each((i, e) => {
-                $(e).find('label').each((i,e) => {
-                    if ($(e).text() === name) {
-                        $(e).parent().find('input').prop('checked', true);
-                        $(this).parent().find('input').prop('checked', false);
-                    }
-                })
-            })
-        })
-    }
-});
-
-
-
-$('#settings').find("fieldset").each((i, e)=>{
-    $(e).find('div').each((i, e) => {
-        let check = $(e).find('.checkbox').is(':checked');
-        let name = $(e).find('label').text().replace(/ /g,'').replace(/(\r\n|\n|\r)/gm, "");
-        $(e).find('.checkbox').attr('data-check', check);
-        $(e).find('.checkbox').attr('data-name', name);
-    })
-});
-
-
-$(".checkbox").each((i, e)=>{
-    let name = $(e).attr("data-name");
-    let c = $("#settings").find("input[data-name='" + name + "']");
-
-    if ($(c[0]).attr("data-check") === $(c[1]).attr("data-check") && $(c[0]).attr("data-check") === "true") {
-        $(c[1]).prop('checked', false)
-    } else if ($(c[0]).attr("data-check") === $(c[1]).attr("data-check") && $(c[0]).attr("data-check") === "false") {
-        $(c[1]).prop('checked', true)
-    }
-});
+checkSelectAll()
