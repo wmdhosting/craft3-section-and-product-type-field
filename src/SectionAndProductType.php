9a15f0e005816ba8a2580e2c077556ff12379d47
Majstor <76176291+wmdhosting@users.noreply.github.com>
2024-03-26 18:46:22 +0200

    Update SectionAndProductType.php

--- src/SectionAndProductType.php
+++ src/SectionAndProductType.php
@@ -24,7 +24,7 @@ class SectionAndProductType extends Plugin
     /**
      * @var string
      */
-    public string $schemaVersion = '2.0.7';
+    public string $schemaVersion = '2.0.8';
 
     /**
      * @var bool
